4e36a944a4b3c3e5aec5c7810871b0bdcd5fda06
Rajangane <pratiksharajangane7@gmail.com>
2022-01-13 13:03:01 +0530

    [Pratiksha] ADD : Retriving Top 3 Records from list whos rating is high using Linq

---
+++ ProductReviewMang_LINQ/ProductReviewMang_LINQ/Management.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewMang_LINQ
+{
+    public class Management
+    {
+        public readonly DataTable datatable = new DataTable();
+        /// <summary>
+        ///   UC2 : Retriving Top 3 Records from list whos rating is high using Linq
+        /// </summary>
+        /// <param name="listProductReview"></param>
+        public void TopRecords(List<ProductReview> listProductReview)
+        {
+            var recordedData = (from productReviews in listProductReview
+                                orderby productReviews.Rating descending
+                                select productReviews).Take(3);
+
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductID:- " + list.ProducID + " " + "UserID:- " + list.UserID
+                     + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike:- " + list.isLike);
+            }
+        }
+
+    }
+}
--- ProductReviewMang_LINQ/ProductReviewMang_LINQ/Program.cs
+++ ProductReviewMang_LINQ/ProductReviewMang_LINQ/Program.cs
@@ -1,31 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("************* Welcome To Product Review Management *****************");
 using ProductReviewMang_LINQ;
-Console.Write("Select Number:\n1)CreateProducts");
+Console.Write("Select Number:\n1)CreateProducts\n2)RetriveTop3");
 int option = Convert.ToInt32(Console.ReadLine());
+List<ProductReview> productReviewList = new List<ProductReview>()
+{
+    new ProductReview(){ProducID=1,UserID=1,Rating=2,Review="Good",isLike=true},
+    new ProductReview(){ProducID=2,UserID=1,Rating=4,Review="Good",isLike=true},
+    new ProductReview(){ProducID=3,UserID=1,Rating=5,Review="Good",isLike=true},
+    new ProductReview(){ProducID=4,UserID=1,Rating=6,Review="Good",isLike=false},
+    new ProductReview(){ProducID=5,UserID=1,Rating=2,Review="nice",isLike=true},
+    new ProductReview(){ProducID=6,UserID=1,Rating=1,Review="bad",isLike=true},
+    new ProductReview(){ProducID=7,UserID=1,Rating=10,Review="Good",isLike=false},
+    new ProductReview(){ProducID=8,UserID=1,Rating=9,Review="nice",isLike=true},
+    new ProductReview(){ProducID=9,UserID=1,Rating=10,Review="nice",isLike=true},
+    new ProductReview(){ProducID=10,UserID=1,Rating=8,Review="nice",isLike=true},
+};
 switch (option)
 {
+
     case 1:
         //UC1
-        List<ProductReview> productReviewList = new List<ProductReview>()
-        {
-            new ProductReview(){ProducID=1,UserID=1,Rating=2,Review="Good",isLike=true},
-            new ProductReview(){ProducID=2,UserID=1,Rating=4,Review="Good",isLike=true},
-            new ProductReview(){ProducID=3,UserID=1,Rating=5,Review="Good",isLike=true},
-            new ProductReview(){ProducID=4,UserID=1,Rating=6,Review="Good",isLike=false},
-            new ProductReview(){ProducID=5,UserID=1,Rating=2,Review="nice",isLike=true},
-            new ProductReview(){ProducID=6,UserID=1,Rating=1,Review="bad",isLike=true},
-            new ProductReview(){ProducID=7,UserID=1,Rating=10,Review="Good",isLike=false},
-            new ProductReview(){ProducID=8,UserID=1,Rating=9,Review="nice",isLike=true},
-            new ProductReview(){ProducID=9,UserID=1,Rating=10,Review="nice",isLike=true},
-            new ProductReview(){ProducID=10,UserID=1,Rating=8,Review="nice",isLike=true},
-
-        };
-
         foreach (var list in productReviewList)
         {
             Console.WriteLine("ProductID:- " + list.ProducID + " " + "UserID:- " + list.UserID
-                + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike:- " + list.isLike);
+            + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike:- " + list.isLike);
         }
+        break;
+    case 2:
+        //UC2
+        Management management = new Management();
+        management.TopRecords(productReviewList);
+        break;
+    case 3:
+        Console.WriteLine("Invalid Option");
         break;
 }
